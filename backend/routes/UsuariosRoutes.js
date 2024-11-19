d5909430fd4ee0e7040e7ad9eb8091763e1cf66d
Julianignacio <montenegrojulianig@gmail.com>
2024-11-19 19:52:23 -0300

    se comento funcion register para posterior mejora

--- backend/routes/UsuariosRoutes.js
+++ backend/routes/UsuariosRoutes.js
@@ -2,11 +2,11 @@ const express = require ("express")
 
 const router = express.Router()
 
-const {AllUsers,login,register} = require ("../controllers/UsuarioControllers")
+const {AllUsers,login} = require ("../controllers/UsuarioControllers")
 // peticiones http
 router.get("/Usuarios",AllUsers)
 router.post("/Usuarios/inicio-sesion",login)
-router.post("/Usuarios/registrar",register)
+// router.post("/Usuarios/registrar",register)
 
 
 module.exports = router
