d3c2ec7d88e26d0b52818d3cd84184f9343896b3
JulianIgnacio <montenegrojulianig@gmail.com>
2025-08-25 20:22:22 -0300

    se establecio el controlador de la tabla subproveedor

---
+++ backend/controllers/SubproveedorCrontollers.js
@@ -0,0 +1,45 @@
+const {conection} = require("../config/DB")
+
+const listarSubProveedores = async (req,res) => {
+    const query = `select * from subproveedores where disponibleSP = 1`
+     conection.query(query,(err,results)=>{
+        if (err) throw err;
+        res.json(results)
+     })
+}
+const buscarSubProveedor = (req,res)=>{
+    const id = req.params.id
+    const query = `select * from subproveedores where id_subproveedor='${id}'`
+    conection.query(query,(err,results)=>{
+        if (err) throw err;
+        res.json(results)
+    })
+}
+const agregarSubProveedor = (req,res) => {
+    const {id_cliente,razon_social_subproveedor,cuit_subproveedor} =req.body 
+
+    const query = `insert into subproveedores (id_cliente,razon_social_subproveedor,cuit_subproveedor) values ('${id_cliente}','${razon_social_subproveedor}','${cuit_subproveedor}')`
+
+    conection.query(query,(err,results)=> {
+        if (err) throw err
+        res.send(results)
+    })
+}
+const actualizarSubProveedor = (req,res) => {
+    const id= req.params.id
+    const {id_cliente,razon_social_subproveedor,cuit_subproveedor} =req.body 
+    const query = `update subproveedores set id_cliente='${id_cliente}',razon_social_subproveedor='${razon_social_subproveedor}',cuit_subproveedor='${cuit_subproveedor}' where id_subproveedor='${id}'`
+    conection.query(query,(err,results)=> {
+        if (err) throw err
+        res.send(results)
+    })
+}
+const eliminarSubProveedor = (req,res) => {
+    const id= req.params.id
+    const query= `update subproveedores set disponibleSP= 0 where id_subproveedor='${id}'`
+    conection.query(query,(err,results)=> {
+        if (err) throw err
+        res.send(results)
+    })
+}
+module.exports = {listarSubProveedores,agregarSubProveedor,actualizarSubProveedor,eliminarSubProveedor,buscarSubProveedor}
