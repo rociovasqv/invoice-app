9d57d45553f1041db859a1a3451b39a7d9aabb8d
JulianIgnacio <montenegrojulianig@gmail.com>
2025-08-26 18:51:21 -0300

    se genero tabla de subproveedores y estructura pagina proveedores on peticiones

---
+++ frontend/src/modules/subproveedores/SubproveedoresPage.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import TablaSubproveedores from "../components/TablaSubproveedores";
+
+
+const SubproveedoresPage = () => {
+  return (
+    <Container fluid>
+        <TablaSubproveedores/>
+    </Container>
+  )
+}
+
+export default SubproveedoresPage;
