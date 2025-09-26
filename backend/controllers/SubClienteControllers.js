ff506adb1293b5eafa026fb04e02af5700880ba3
JulianIgnacio <montenegrojulianig@gmail.com>
2025-09-26 20:14:28 -0300

    se reestructuro el controlador de la tabla subclientes para que tome de referencia el id del cliente

--- backend/controllers/SubClienteControllers.js
+++ backend/controllers/SubClienteControllers.js
@@ -1,8 +1,8 @@
 const {conection} = require("../config/DB")
 
 const listarSubClientes = async (req,res) => {
-    const id = req.params.id
-    const query = `select * from subclientes where disponibleS = 1 and id_cliente='${id}'`
+    const idCliente = req.params.idCliente
+    const query = `select * from subclientes where disponibleS = 1 and id_cliente='${idCliente}'`
      conection.query(query,(err,results)=>{
         if (err) throw err;
         res.json(results)
@@ -18,9 +18,11 @@ const buscarSubCliente = (req,res)=>{
     })
 }
 const agregarSubCliente = (req,res) => {
-    const {id_cliente,razon_social_subcliente,cuit_subcliente} =req.body 
+    const idCliente =  req.params.idCliente
 
-    const query = `insert into subclientes (id_cliente,razon_social_subcliente,cuit_subcliente) values ('${id_cliente}','${razon_social_subcliente}','${cuit_subcliente}')`
+    const {razon_social_subcliente,cuit_subcliente} =req.body 
+
+    const query = `insert into subclientes (id_cliente,razon_social_subcliente,cuit_subcliente) values ('${idCliente}','${razon_social_subcliente}','${cuit_subcliente}')`
 
     conection.query(query,(err,results)=> {
         if (err) throw err
@@ -29,17 +31,19 @@ const agregarSubCliente = (req,res) => {
 }
 const editarSubCliente = (req,res) => {
 
+    const idCliente =  req.params.idCliente
     const id= req.params.id
-    const {id_cliente,razon_social_subcliente,cuit_subcliente} =req.body 
-    const query = `update subclientes set id_cliente='${id_cliente}' , razon_social_subcliente='${razon_social_subcliente}' ,cuit_subcliente='${cuit_subcliente}' where id_subcliente='${id}'`
+    const {razon_social_subcliente,cuit_subcliente} =req.body 
+    const query = `update subclientes set razon_social_subcliente='${razon_social_subcliente}' ,cuit_subcliente='${cuit_subcliente}' where id_subcliente='${id}' and id_cliente='${idCliente}'`
     conection.query(query,(err,results)=> {
         if (err) throw err
         res.send(results)
     })
 }
 const eliminarSubCliente = (req,res) => {
+    const idCliente =  req.params.idCliente
     const id= req.params.id
-    const query= `update subclientes set disponibleS= 0 where id_subcliente='${id}'`
+    const query= `update subclientes set disponibleS= 0 where id_subcliente='${id}' and id_cliente='${idCliente}'`
     conection.query(query,(err,results)=> {
         if (err) throw err
         res.send(results)
