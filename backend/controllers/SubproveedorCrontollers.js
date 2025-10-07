c70bf3d329124669c37c6b15b100339b22114044
JulianIgnacio <montenegrojulianig@gmail.com>
2025-10-06 21:28:04 -0300

    se genero la estructura para el backend

--- backend/controllers/SubproveedorCrontollers.js
+++ backend/controllers/SubproveedorCrontollers.js
@@ -1,24 +1,28 @@
 const {conection} = require("../config/DB")
 
 const listarSubProveedores = async (req,res) => {
-    const query = `select * from subproveedores where disponibleSP = 1`
+    const idCliente = req.params.idCliente
+    const query = `select * from subproveedores where id_cliente = '${idCliente}' and disponibleSP = 1`
      conection.query(query,(err,results)=>{
         if (err) throw err;
         res.json(results)
      })
 }
 const buscarSubProveedor = (req,res)=>{
+    const idCliente =  req.params.idCliente
     const id = req.params.id
-    const query = `select * from subproveedores where id_subproveedor='${id}'`
+    const query = `select * from subproveedores where id_subproveedor='${id}' and id_cliente = '${idCliente}' and disponibleSP = 1`
     conection.query(query,(err,results)=>{
         if (err) throw err;
         res.json(results)
     })
 }
 const agregarSubProveedor = (req,res) => {
-    const {id_cliente,razon_social_subproveedor,cuit_subproveedor} =req.body 
 
-    const query = `insert into subproveedores (id_cliente,razon_social_subproveedor,cuit_subproveedor) values ('${id_cliente}','${razon_social_subproveedor}','${cuit_subproveedor}')`
+    const idCliente =  req.params.idCliente
+    const {razon_social_subproveedor,cuit_subproveedor} =req.body 
+
+    const query = `insert into subproveedores (id_cliente,razon_social_subproveedor,cuit_subproveedor) values ('${idCliente}','${razon_social_subproveedor}','${cuit_subproveedor}')`
 
     conection.query(query,(err,results)=> {
         if (err) throw err
@@ -26,17 +30,19 @@ const agregarSubProveedor = (req,res) => {
     })
 }
 const actualizarSubProveedor = (req,res) => {
+    const idCliente =  req.params.idCliente
     const id= req.params.id
-    const {id_cliente,razon_social_subproveedor,cuit_subproveedor} =req.body 
-    const query = `update subproveedores set id_cliente='${id_cliente}',razon_social_subproveedor='${razon_social_subproveedor}',cuit_subproveedor='${cuit_subproveedor}' where id_subproveedor='${id}'`
+    const {razon_social_subproveedor,cuit_subproveedor} =req.body 
+    const query = `update subproveedores set id_cliente='${idCliente}',razon_social_subproveedor='${razon_social_subproveedor}',cuit_subproveedor='${cuit_subproveedor}' where id_subproveedor='${id}'`
     conection.query(query,(err,results)=> {
         if (err) throw err
         res.send(results)
     })
 }
 const eliminarSubProveedor = (req,res) => {
+    const idCliente =  req.params.idCliente
     const id= req.params.id
-    const query= `update subproveedores set disponibleSP= 0 where id_subproveedor='${id}'`
+    const query= `update subproveedores set disponibleSP= 0 where id_subproveedor='${id}' and id_cliente = '${idCliente}'`
     conection.query(query,(err,results)=> {
         if (err) throw err
         res.send(results)
