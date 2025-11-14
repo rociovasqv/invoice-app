751b81e92662bd04c6bb73329d180032598123f7
JulianIgnacio <montenegrojulianig@gmail.com>
2025-11-13 22:46:16 -0300

    se configuro backend para usar variables de entorno

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -1,11 +1,13 @@
 const mysql = require ("mysql2")
+require('dotenv').config()
 
 const conection = mysql.createConnection(
     {
-        host: "localhost",
-        user: "root",
-        password: "1234",
-        database:"ampuero"
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DB
     }
 )
 
