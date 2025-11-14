1337d8c887a0fd43a7a05a31453bd1f781a56db1
JulianIgnacio <montenegrojulianig@gmail.com>
2025-11-13 22:48:39 -0300

    se apunto a db local

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 
 const conection = mysql.createConnection(
     {
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DB
+        host: process.env.DATABASE_LOCAL_HOST,
+        port: process.env.DATABASE_LOCAL_PORT,
+        user: process.env.DATABASE_LOCAL_USERNAME,
+        password: process.env.DATABASE_LOCAL_PASSWORD,
+        database: process.env.DATABASE_LOCAL_DB
     }
 )
 
